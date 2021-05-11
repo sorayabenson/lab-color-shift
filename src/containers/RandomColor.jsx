dd8486da81fc02c84287f61a562461358fc36b12
Soraya Benson <soraya.benson@gmail.com>
2021-05-10 18:04:56 -0700

    functionality half way there

--- src/containers/RandomColor.jsx
+++ src/containers/RandomColor.jsx
@@ -2,10 +2,31 @@ import React, { Component } from 'react';
 import ColorCube from '../components/color/ColorCube';
 
 export default class RandomColor extends Component {
+    state = {
+        color: '#FFD426',
+    }
+
+    colorArray = ['#FFD426', '#FEB83A', '#FD9C4F', '#FC8063', '#FB6377', '#FA478C', '#F92BA0'];
+    
+    shuffle = (array) => {
+        //pick random i from array
+    }
+
+    handleChange = (e) => {
+        //setState with random color from colorArray every second
+        newColor = shuffle(colorArray)
+        
+        this.setState({ color: e.target.newColor })
+    }
+
     render() {
+        const { color } = this.state;
+
         return (
             <div>
-                <ColorCube />
+                <ColorCube 
+                    color={color}
+                    onChange={this.handleChange}/>
             </div>
         )
     }
